83f1f1ccd3accc5807901c30266943d69713cb56
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-20 14:57:43 +0700

    finished build database

--- src/main/java/com/example/estore/EStoreApplication.java
+++ src/main/java/com/example/estore/EStoreApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EStoreApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EStoreApplication.class, args);
+    }
 
 }
