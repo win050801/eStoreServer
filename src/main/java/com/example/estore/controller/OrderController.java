5115ec8d25a0c0ac3ec997ec2229886aa598d7aa
CaoThang <tcao2315@gmail.com>
2022-11-23 14:39:02 +0700

    update OrderController

--- src/main/java/com/example/estore/controller/OrderController.java
+++ src/main/java/com/example/estore/controller/OrderController.java
@@ -3,10 +3,9 @@ package com.example.estore.controller;
 import com.example.estore.entity.Order;
 import com.example.estore.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -20,4 +19,10 @@ public class OrderController {
 
         return orderRepository.save(order);
     }
+    @PostMapping("getOrder/{id}")
+    public List<Order> getOrder(@PathVariable String id)
+    {
+        return orderRepository.findByCustomerId(id);
+    }
+
 }
