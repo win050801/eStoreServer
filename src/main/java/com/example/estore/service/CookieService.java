aa2a010bf0bf569b9de97e9d40fa7a20197f48df
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-23 15:41:38 +0700

    delete bean

---
+++ src/main/java/com/example/estore/service/CookieService.java
@@ -0,0 +1,44 @@
+package com.example.estore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
+
+@Service
+public class CookieService {
+    @Autowired
+    HttpServletRequest request;
+    @Autowired
+    HttpServletResponse response;
+
+    public Cookie create(String name,String value,int days){
+
+        String encodedValue = Base64.getEncoder().encodeToString(value.getBytes());
+        Cookie cookie = new Cookie(name,encodedValue);
+        cookie.setMaxAge(days*24*60*60);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return cookie;
+    }
+    public Cookie read(String name){
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equalsIgnoreCase(name)){
+                    String decodedValue = new String(Base64.getDecoder().decode(cookie.getValue()));
+                    cookie.setValue(decodedValue);
+                    return cookie;
+                }
+            }
+        }
+        return null;
+    }
+    public void delete(String name){
+        this.create(name,"",0);
+    }
+
+}
